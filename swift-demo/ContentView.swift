2e9f919bd2dd41ac15dc73bf01de02a1b7254e39
Eunika Wu <ewu@laika.com>
2021-06-17 18:10:21 +0000

    Init

---
+++ swift-demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swift-demo
+//
+//  Created by Eunika Wu on 5/26/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
